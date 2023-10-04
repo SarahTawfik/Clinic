0410fb0e222cc0bd74ac5ecb49aecf06c00ef449
SarahTawfik <sarahtawfik0447@gmail.com>
2023-10-04 16:52:44 +0300

    Initial

---
+++ Backend/src/Models/Adminstrator.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const adminstratorSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
+
+const User = mongoose.model('Adminstrator', adminstratorSchema);
+module.exports = Adminstrator;
